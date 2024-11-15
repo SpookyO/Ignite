c4fbd7926e0396c504a2aec67af3b3ebf208dcc5
SpookyO <iqbalbroo24@gmail.com>
2024-11-15 19:43:56 +0600

    add ignite logo

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,18 +1,24 @@
 "use client"
 import React from 'react'
 import { useState } from 'react';
+import Image from 'next/image'
 
 function NavBar() {
     const [isOpen, setIsOpen] = useState(false);
     const [menuOne, setMenuOne] = useState(false);
   
     return (
-      <section>
-        <nav className="font-inter mx-auto h-auto w-full max-w-screen-2xl lg:relative lg:top-0">
-          <div className="flex flex-col px-6 py-6 lg:flex-row lg:items-center lg:justify-between lg:px-10 lg:py-4 xl:px-20">
-            <a href="#">
-              <div className="text-4xl font-bold">Ignite</div>
-            </a>
+<section>
+  <nav className="font-inter mx-auto h-auto w-full max-w-screen-2xl lg:relative lg:top-0">
+     
+        <div className="flex items-center space-x-3 px-6 py-6 lg:flex-row lg:items-center lg:justify-between lg:px-10 lg:py-4 xl:px-20">
+        
+             
+            
+            <a href="#" className="w-max h-max">  
+                <div className="text-4xl font-bold" >Ignite</div>
+            </a> 
+ 
             <div
               className={`mt-14 flex flex-col space-y-8 lg:mt-0 lg:flex lg:flex-row lg:space-x-1 lg:space-y-0 ${isOpen ? "" : "hidden"}`}
             >
@@ -101,7 +107,7 @@ function NavBar() {
                   
                   </div>
                 )}
-              </div>
+              </div> 
               <a
                 href="#"
                 className="font-inter rounded-lg lg:px-6 lg:py-4 lg: lg:hover:text-gray-800"
